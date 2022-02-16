fb5964181556049f19b9c6a4c9f842364f7144fb
Nauval Eka <mnaauval20@gmail.com>
2022-02-16 19:50:09 +0700

    Update productsApi.js

--- src/features/productsApi.js
+++ src/features/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://fourty-shop.herokuapp.com/" }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => "products",
